fdbd3845d7ddad550ff0965f3ebd3a53b943b4e7
themysticsavages <adijoshi08@gmail.com>
2022-08-04 06:06:06 -0700

    app stuff

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from src.api.routes.api import predict
+from src.api.routes.api import predict, ytscrape
 from src.api.routes import root
 
 app = FastAPI()
 
 app.include_router(root.router)
 app.include_router(predict.router)
+app.include_router(ytscrape.router)
