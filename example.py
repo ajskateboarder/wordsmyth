5f5c50bc49bede3de93270a2e8c956ee5bf653aa
themysticsavages <adijoshi08@gmail.com>
2022-08-10 06:13:26 -0700

    server prog

--- example.py
+++ example.py
@@ -1,25 +1,45 @@
+from concurrent.futures import as_completed
 import streamlit as st
-import requests
+
+from requests_futures.sessions import FuturesSession
 
 from src.ytd import get_comments
-from src.deepmoji import Emojize
 
-emoji = Emojize()
+
+def chunk(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+session = FuturesSession()
 
 with st.form("form"):
     yt_id = st.text_input("YouTube video ID")
 
     submitted = st.form_submit_button()
     if submitted:
-        res = requests.get(
+        res = session.get(
             f"https://youtube.com/oembed?url=https://youtube.com/watch?v={yt_id}"
-        )
+        ).result()
         if res.status_code == 400:
             st.error("Video doesn't exist. Try a working video ID.")
 
         title = res.json()["title"]
         st.write(f'Downloading comments for "{title}". Please wait...')
 
-        comments = get_comments(yt_id)
-        for comment in comments:
-            st.write(comment, emoji.predict(comment))
+        texts = [i for i in get_comments(yt_id, 100)]
+        chunks = list(chunk(texts, 20))
+
+        futures = [
+            session.get(
+                f"http://localhost:800{i}/predict",
+                headers={"texts": str(c).encode("utf-8")},
+            )
+            for i, c in zip(list(range(6))[1:], chunks)
+        ]
+
+        for future in as_completed(futures):
+            resp = future.result().json()
+            for re in resp:
+                st.write(re["text"], re["emoji"])
