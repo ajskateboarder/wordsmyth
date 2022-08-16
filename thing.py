fa5b96c0ef33802b8239cb5bbcb1bcab27b3a71b
themysticsavages <adijoshi08@gmail.com>
2022-08-16 13:04:34 -0700

    more tiny stuff

--- thing.py
+++ thing.py
@@ -1,9 +1,7 @@
 import json
+import itertools
 
 with open("data.json") as fh:
     data = json.load(fh)
 
-d: "list[str, str]"
-for d in data:
-    if not d[1].startswith("'\\xa0@") or not d[1].startswith('"'):
-        print(d[1])
+ab = list(itertools.chain(*[e for e in data]))
