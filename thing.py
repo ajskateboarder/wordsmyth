c6539a2090ea0fb8bfe8b06b71cb8692402178ab
themysticsavages <adijoshi08@gmail.com>
2022-08-28 05:59:26 -0700

    convert scripts to python

--- thing.py
+++ thing.py
@@ -1,11 +1,17 @@
 import json
 import itertools
+from emoji import emojize
 
 with open("data.json") as fh:
     data = json.load(fh)
+
+with open("esranks.json") as fh:
+    emojis: "list[dict]" = json.load(fh)
+    classes = [e["emoji_repr"] for e in emojis]
 
 ab = list(itertools.chain(*[e for e in data]))
 
 for emoji in ab:
     if not emoji["text"].startswith("\xa0@"):
         print(emoji["text"].strip(), emoji["emoji"], "\n")
+        print(emojis[classes.index(emoji["emoji"])])
