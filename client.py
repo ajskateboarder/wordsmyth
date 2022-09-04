b873df1a3f7d732bb52233d8dff1bb3811db1444
themysticsavages <adijoshi08@gmail.com>
2022-09-04 16:24:50 -0700

    migration to grpc [wip]

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import grpc
 
-from microv2.server_pb2 import emojis, texts
+from microv2.server_pb2 import sentiments, texts
 from microv2.server_pb2_grpc import ModelStub
 
 print("Connecting to gRPC server")
@@ -10,7 +10,7 @@ channel = grpc.insecure_channel("localhost:50051")
 
 
 def main(nlps):
-    fetch = ModelStub(channel).torchmoji
+    fetch = ModelStub(channel).roberta
 
     print("Requesting comments")
 
@@ -19,7 +19,7 @@ def main(nlps):
 
     for f in as_completed(futures):
         print("Collected")
-        print([e.split(", ") for e in f.result().emojis])
+        print([[float(g) for g in e.split(", ")] for e in f.result().sentiments])
 
 
 main([["This is the shit!", "You are shit."], ["Nice", "Very cool"]])
