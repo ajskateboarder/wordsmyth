303cf14271b3451d5733d17d783e158a3521626e
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-09-07 01:03:11 +0000

    commit

--- tests/micro/test_torchmoji.py
+++ tests/micro/test_torchmoji.py
@@ -1,5 +1,6 @@
 import pytest
 from grpc._channel import _InactiveRpcError
+from google.protobuf.json_format import MessageToDict
 
 from microv2.stubs.server_pb2 import Texts
 
@@ -30,16 +31,29 @@ def test_none(grpc_stub):
     """Test if an empty response returns an empty list as there's nothing to process"""
 
     request = Texts()
-    response = grpc_stub.torchmoji(request)
+    response = MessageToDict(grpc_stub.torchmoji(request))
 
-    assert response.emojis == []
+    assert response == {}
+
+
+@pytest.mark.filterwarnings("ignore::UserWarning", "ignore::DeprecationWarning")
+def test_some(grpc_stub, capsys):
+    """Test some sample texts"""
+
+    request = Texts(texts=["This isn't too bad!", "Nice tutorial"])
+    response = MessageToDict(grpc_stub.torchmoji(request))
+
+    with capsys.disabled():
+        print(response)
+
+    assert True
 
 
 @pytest.mark.filterwarnings("ignore::UserWarning", "ignore::DeprecationWarning")
 def test_exception(grpc_stub):
     """Test if using a non-list type raises an exception"""
 
-    request = texts(texts="Hello world")
+    request = Texts(texts="Hello world")
 
     with pytest.raises(_InactiveRpcError) as exc:
         grpc_stub.torchmoji(request)
