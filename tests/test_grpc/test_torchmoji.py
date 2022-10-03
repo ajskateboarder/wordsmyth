a3b06b39fffb8ee45a68caa3c083129836e89ca0
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-10-03 10:56:46 +0000

    add pydantic to requirements.txt

--- tests/test_grpc/test_torchmoji.py
+++ tests/test_grpc/test_torchmoji.py
@@ -1,11 +1,8 @@
 """Test Torchmoji gRPC API"""
 
-from typing import get_type_hints
-
 import pytest
 from grpc._channel import _InactiveRpcError
 from google.protobuf.json_format import MessageToDict
-from pydantic.error_wrappers import ValidationError # pylint: disable=no-name-in-module
 
 from micro.stubs.server_pb2 import Request
 from .classes import Torch
@@ -27,9 +24,6 @@ def test_some(grpc_stub, capsys):
 
     request = Request(texts=["This isn't too bad!", "Nice tutorial"], count=2)
     response = MessageToDict(grpc_stub.torchmoji(request))
-
-    with capsys.disabled():
-        print(response)
 
     assert response["response"]
     for item in response["response"]:
