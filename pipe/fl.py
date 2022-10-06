543872a81a30d4c0a309d58a77ba02a7eaef8bc2
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-10-06 00:50:53 +0000

    more piping stuff

--- pipe/fl.py
+++ pipe/fl.py
@@ -5,38 +5,50 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from google.protobuf.json_format import MessageToDict
 import grpc
 
-from modelparser import Model
 from micro.stubs.server_pb2 import Request
 from micro.stubs.server_pb2_grpc import ModelStub
 
 
-channel = grpc.insecure_channel("localhost:50051")
+_channel = grpc.insecure_channel("localhost:50051")
 
 
 def submit_futures(content):
     with ThreadPoolExecutor() as executor:
         futures = [
-            executor.submit(ModelStub(channel).torchmoji, request=Request(texts=v))
+            executor.submit(ModelStub(_channel).flair, request=Request(texts=v))
             for v in content
         ]
     return as_completed(futures)
 
 
 def main(texts):
-    print("[", end="")
+    data = []
 
     responses = submit_futures(texts)
 
     for f in responses:
         future = MessageToDict(f.result())
         for item in future["response"]:
-            print(item, end=",")
+            data.append(item)
 
-    print("]", end="")
+    return data
 
 
 if __name__ == "__main__":
     from ast import literal_eval
+    from pprint import pprint
+    import argparse
+    import json
     import sys
 
-    main(literal_eval(list(sys.stdin)[-1]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pretty", "-p", action="store_true", help="Format JSON")
+    
+    pretty = parser.parse_args().pretty
+    data = main(literal_eval(list(sys.stdin)[-1]))
+    
+    if pretty:
+        pprint(data)
+    else:
+        print(json.dumps(data))
+    
