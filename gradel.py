71a2087e399e3d23aa802acfc94be3b44fd03b2b
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-10-13 11:51:46 -0400

    Delete gradel.py

--- gradel.py
+++
@@ -1,9 +0,0 @@
-from gradio import Blocks, Textbox, Button, Label
-
-with Blocks(title="test app") as app:
-    tb = Textbox("Hello")
-    button = Button("world")
-    label = Label("Value")
-    button.click(fn=lambda x: label.update(tb.value), inputs=[tb], outputs=[label])
-
-app.launch()
