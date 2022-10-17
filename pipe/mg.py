7f84aa40c3fc290eb0dfa16a5bc5c10e8c1a41c8
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-10-17 01:16:14 +0000

    more data merging stuff

--- pipe/mg.py
+++ pipe/mg.py
@@ -4,5 +4,13 @@ import sys
 import ast
 
 inputs = list(sys.stdin)
-for i in inputs:
-    print(ast.literal_eval(i.strip()))
+print(inputs)
+
+# flair = ast.literal_eval(ast.literal_eval(inputs[0].strip())[0])
+# torch = ast.literal_eval(inputs[-1])
+
+# merged = [dict(t, **f) for t, f in zip(torch, flair)]
+
+# print("sentiment,score,text,emojis")
+# for m in merged:
+#     print(f"{m['sentiment']['sentiment']},{m['sentiment']['score']},'{m['text']}','{','.join(m['emojis'])}'")
