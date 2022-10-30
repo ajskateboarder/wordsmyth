766fca1d537d83b07e6f9ba252abc46d25bf9d25
themysticsavages <adijoshi08@gmail.com>
2022-10-29 18:53:34 -0700

    more stuff

--- pipe/mg.py
+++ pipe/mg.py
@@ -12,4 +12,6 @@ merged = [dict(t, **f) for t, f in zip(torch, flair)]
 
 print("sentiment,score,text,emojis")
 for m in merged:
-    print(f"{m['sentiment']['sentiment']},{m['sentiment']['score']},'{m['text']}','{','.join(m['emojis'])}'")
+    print(
+        f"{m['sentiment']['sentiment']},{m['sentiment']['score']},'{m['text']}','{','.join(m['emojis'])}'"
+    )
