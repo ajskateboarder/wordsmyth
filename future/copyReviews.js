1047876a204794ce6c2eb4acd702f664eaa5fe6e
themysticsavages <adijoshi08@gmail.com>
2022-12-30 17:12:33 -0800

    cleanup

--- future/copyReviews.js
+++ future/copyReviews.js
@@ -13,42 +13,48 @@
 // ==/UserScript==
 
 (async function () {
-  var chunk = (arr, len) => {
-    var chunks = [],
-      i = 0,
-      n = arr.length;
+  document.querySelector("#cm_cr-view_opt_search").innerHTML +=
+    "<button id='doSomethingCool'>Collect reviews</button>";
 
-    while (i < n) {
-      chunks.push(arr.slice(i, (i += len)));
+  document.querySelector("#doSomethingCool").onclick = async () => {
+    var chunk = (arr, len) => {
+      var chunks = [],
+        i = 0,
+        n = arr.length;
+
+      while (i < n) {
+        chunks.push(arr.slice(i, (i += len)));
+      }
+      return chunks;
+    };
+
+    var elements = [];
+
+    document
+      .querySelectorAll("span[data-hook='review-body']")
+      .forEach((element) =>
+        elements.push(element.textContent.replace(/(\r\n|\n|\r)/gm, "").trim())
+      );
+
+    var added = [...(await GM_getValue("elements")), elements];
+    await GM_setValue("elements", added);
+
+    var pageNumber = parseInt(
+      new URLSearchParams(window.location.search).get("pageNumber").toString()
+    );
+    if (pageNumber === 10) {
+      document.body.innerHTML = JSON.stringify(
+        // this is for optimal performance when using with the algorithms
+        chunk((await GM_getValue("elements")).flat(Infinity), 5)
+      );
+      await GM_setValue("elements", []);
+      throw new Error("Hello world");
     }
-    return chunks;
+    setTimeout(
+      () =>
+        (window.location =
+          window.location.href.slice(0, -1) + (pageNumber + 1)),
+      1000
+    );
   };
-
-  var elements = [];
-
-  document
-    .querySelectorAll("span[data-hook='review-body']")
-    .forEach((element) =>
-      elements.push(element.textContent.replace(/(\r\n|\n|\r)/gm, "").trim())
-    );
-
-  var added = [...(await GM_getValue("elements")), elements];
-  await GM_setValue("elements", added);
-
-  var pageNumber = parseInt(
-    new URLSearchParams(window.location.search).get("pageNumber").toString()
-  );
-  if (pageNumber === 5) {
-    document.body.innerHTML = JSON.stringify(
-      // this is for optimal performance when using with the algorithms
-      chunk((await GM_getValue("elements")).flat(Infinity), 5)
-    );
-    await GM_setValue("elements", []);
-    throw new Error("Hello world");
-  }
-  setTimeout(
-    () =>
-      (window.location = window.location.href.slice(0, -1) + (pageNumber + 1)),
-    1000
-  );
 })();
