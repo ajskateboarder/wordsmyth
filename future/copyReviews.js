5cec6e719f004e619731182ae5c9e5ed4ddee51e
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2023-02-01 14:41:15 -0500

    improve copy script to make more integrated

--- future/copyReviews.js
+++ future/copyReviews.js
@@ -1,15 +1,12 @@
 // ==UserScript==
 // @name         Amazon review collector
 // @namespace    https://www.amazon.com
-// @version      0.1
+// @version      0.2
 // @description  Development stuff
 // @author       themysticsavages
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @match        https://www.amazon.com/*/product-reviews/*
-// @match        https://html.duckduckgo.com/html/
 // @run-at       document-end
-// @grant        GM_getValue
-// @grant        GM_setValue
 // ==/UserScript==
 
 (async function () {
@@ -18,19 +15,20 @@
 
   if (params.get("pageNumber") === null) {
     throw new Error(
-      "ARC: Page number not specified, define a page start and page end with ?pageNumber=2&pageMacro=5"
+      "[arc] Page number not specified, define a page start and page end with ?pageNumber=2&pageMacro=5"
     );
   }
   if (params.get("pageMacro") === null) {
     throw new Error(
-      "ARC: Pages to index were not specified, define a page start and page end with ?pageNumber=2&pageMacro=5"
+      "[arc] Pages to index were not specified, define a page start and page end with ?pageNumber=2&pageMacro=5"
     );
   }
   if (params.get("noChunk") !== null) {
-    console.log("ARC: Chunking is disabled by ?noChunk");
+    console.info("[arc] Chunking is disabled by ?noChunk");
     chunked = false;
-  } else
-    console.log("ARC: Chunking is enabled. This can be disabled with ?noChunk");
+  } else {
+    console.info("[arc] Chunking is enabled. This can be disabled with ?noChunk");
+  }
 
   var chunk = (arr, len) => {
     var chunks = [],
@@ -51,18 +49,62 @@
       elements.push(element.textContent.replace(/(\r\n|\n|\r)/gm, "").trim())
     );
 
-  var added = [...(await GM_getValue("elements")), elements];
-  await GM_setValue("elements", added);
+  var added = [...JSON.parse(await localStorage.getItem("elements")), elements];
+  await localStorage.setItem("elements", JSON.stringify(added));
 
   var pageNumber = parseInt(params.get("pageNumber"));
   if (pageNumber === parseInt(params.get("pageMacro"))) {
-    document.body.innerHTML = JSON.stringify(
+    var dimCSS = `#dimScreen {
+      position: fixed;
+      padding: 0;
+      margin: 0;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background: rgba(0, 0, 0, 0.5);
+    }`
+    var messageCSS = `#message {
+      position: fixed;
+      top: 50%;
+      right: 50%;
+      transform: translate(50%,-50%);
+      background: #131921;
+      color: white;
+      padding: 10px;
+      overflow-y: auto;
+      width: 50%;
+      height: 50%;
+      z-index: 3;
+    }
+    #ajson {
+      background-color: #232F3E;
+      padding: 5px;
+      margin-top: 10px;
+    }
+    #abutton {
+      border: none;
+      padding: 5px;
+      background-color: #232F3E;
+      color: white;
+    }
+    #abutton:hover { background-color: #304054; }
+    `
+    var copyScript = 'navigator.clipboard.writeText(document.querySelector("#ajson").innerText).then(() => alert("Copied"))'
+    var closeScript = 'document.querySelector("#dimScreen").remove(); document.querySelector("#message").remove()'
+
+    document.head.insertAdjacentHTML("beforeend", `<style>${dimCSS}</style>`)
+    document.head.insertAdjacentHTML("beforeend", `<style>${messageCSS}</style>`)
+    document.body.insertAdjacentHTML("beforeend", "<div id='dimScreen'></div>")
+    document.body.insertAdjacentHTML("beforeend",
+      `<div id='message'><h2>Here's that stuff you wanted</h2><button id='abutton' onclick='${copyScript}'>Copy JSON</button>&nbsp;&nbsp;<button id='abutton' onclick='${closeScript}'>Close</button>
+      <br><pre id='ajson' style='white-space: pre-wrap;'>${JSON.stringify(
       chunked
-        ? chunk((await GM_getValue("elements")).flat(Infinity), 5)
-        : (await GM_getValue("elements")).flat(Infinity)
-    );
-    await GM_setValue("elements", []);
-    throw new Error("Hello world");
+        ? chunk((await JSON.parse(localStorage.getItem("elements"))).flat(Infinity), 5)
+        : (await JSON.parse(localStorage.getItem("elements"))).flat(Infinity)
+    )}</pre></div>`);
+    await localStorage.setItem("elements", JSON.stringify([]));
+    throw new Error("[]");
   }
   params.set("pageNumber", pageNumber + 1);
   setTimeout(
