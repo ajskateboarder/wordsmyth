85784bbb863b195959d3018b9aa4d6f799fb7e3b
ajskateboarder <81255936+lemystic@users.noreply.github.com>
2023-02-24 12:13:56 +0000

    feat: completed async model eval

--- wordsmyth/rate.py
+++ wordsmyth/rate.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import Union, Generator
+import warnings
 from itertools import repeat
-from pathos.multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, Generator, Union
 
 from .rate_utils import fix_content, rate
 
@@ -12,14 +13,19 @@ InputType = Union[str, list[str]]
 
 class Wordsmyth:
     def __init__(self) -> None:
+        warnings.filterwarnings("ignore")
         self._flair = Flair()
         self._torchmoji = TorchMoji()
 
-    def model_eval(self, texts: list[list[str]], emojis: int) -> None:
+    def model_eval(self, texts: list[list[str]], emojis: int) -> Generator[dict[str, Any], None, None]:
         """Evaluate multiple chunks of text through both Flair and TorchMoji"""
-        with Pool() as pool:
-            for flair, torch in zip(pool.map(self.flair, texts), pool.map(self.torchmoji, texts, repeat(emojis))):
-                pass
+        with ThreadPoolExecutor(len(texts)) as pool:
+            with ThreadPoolExecutor(len(texts)) as pool:
+                flair = pool.map(self.flair, texts)
+                torchmoji = pool.map(self.torchmoji, texts, repeat(emojis))
+                for resf_, rest_, text_ in zip(flair, torchmoji, texts):
+                    for resf, rest, text in zip(resf_, rest_, text_):
+                        yield {**resf, "emojis": rest, "text": text}
 
     def flair(self, text: InputType) -> Generator[dict[str, Union[str, float]], None, None]:
         """Evaluate a single text or a list of texts through Flair's `en-sentiment` model"""
